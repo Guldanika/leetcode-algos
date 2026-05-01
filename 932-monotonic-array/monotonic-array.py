054e2a89cf52bedfee703bb82ff640c1251a45e0
Guldanika Osmonova <osmonova.daniko@gmail.com>
2026-05-01 13:49:14 -0500

    Time: 66 ms (23.11%) | Memory: 31.2 MB (81.54%) - LeetSync

--- 932-monotonic-array/monotonic-array.py
+++ 932-monotonic-array/monotonic-array.py
@@ -1,21 +1,32 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
         
-        def inc(nums):
-            prev = - float('inf')
-            for num in nums:
-                if num < prev:
-                    return False
-                prev = num 
-            return True 
+        #def inc(nums):
+        #    prev = - float('inf')
+         #   for num in nums:
+         #       if num < prev:
+          #          return False
+           #     prev = num 
+            #return True 
         
-        def dec(nums):
-            prev = float ('inf')
-            for num in nums: 
-                if num > prev:
-                    return False 
-                prev = num
-            return True 
+        #def dec(nums):
+         #   prev = float ('inf')
+          #  for num in nums: 
+           #     if num > prev:
+            #        return False 
+             #   prev = num
+            #return True 
 
-        return inc(nums) or dec(nums)
+        #return inc(nums) or dec(nums)
 
+        inc = True
+        dec = True 
+
+        for i in range(1, len(nums)):
+            if nums[i] <  nums[i-1]:
+                inc = False
+
+            if nums[i] >  nums[i-1]:
+                dec = False 
+
+        return inc or dec
