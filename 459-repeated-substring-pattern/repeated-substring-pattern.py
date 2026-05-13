e6014773ec3410127ec14fe19ed89d5c0be96e32
Guldanika Osmonova <osmonova.daniko@gmail.com>
2026-05-13 12:06:09 -0500

    Time: 1 ms (68.64%) | Memory: 19.2 MB (63.59%) - LeetSync

---
+++ 459-repeated-substring-pattern/repeated-substring-pattern.py
@@ -0,0 +1,8 @@
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        # Магическая строчка
+        return s in (s + s)[1:-1] 
+
+        #[1:-1]  - срезать 
+        #(s + s) Мы создаем строку в два раза длиннее.
+        #This problem has a very clever solution. Instead of using complex loops, we can use a mathematical property. If we double the string and remove the first and last characters, a repeating pattern will still contain the original string in the middle. In Python, this is just a single line of code using string slicing.
