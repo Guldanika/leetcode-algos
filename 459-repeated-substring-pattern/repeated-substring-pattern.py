da3a3bdb1b0e829b1a0a97b9f1cc31dd327782c2
Guldanika Osmonova <osmonova.daniko@gmail.com>
2026-05-13 12:10:24 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (31.18%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.py
+++ 459-repeated-substring-pattern/repeated-substring-pattern.py
@@ -3,6 +3,6 @@ class Solution:
         # Магическая строчка
         return s in (s + s)[1:-1] 
 
-        #[1:-1]  - срезать 
+        #[1:-1]  - срезать из конечной выборки первую и последнуюю букву, символ 
         #(s + s) Мы создаем строку в два раза длиннее.
         #This problem has a very clever solution. Instead of using complex loops, we can use a mathematical property. If we double the string and remove the first and last characters, a repeating pattern will still contain the original string in the middle. In Python, this is just a single line of code using string slicing.
